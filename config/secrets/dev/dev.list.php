6806a9bbcbf82fd38d4846d9c294957f7a26162e
Norbert Bogucki <nbogucki@zsp5kozuchow.pl>
2021-08-31 21:05:08 +0200

    Initial Commit

---
+++ config/secrets/dev/dev.list.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'DATABASE_HOST' => null,
+    'DATABASE_NAME' => null,
+    'DATABASE_PASSWORD' => null,
+    'DATABASE_URL' => null,
+    'DATABASE_USER' => null,
+];
---
+++ src/Entity/Products.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ProductsRepository::class)
+ */
+class Products
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $sku;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $category;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getSku(): ?string
+    {
+        return $this->sku;
+    }
+
+    public function setSku(string $sku): self
+    {
+        $this->sku = $sku;
+
+        return $this;
+    }
+
+    public function getCategory(): ?int
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?int $category): self
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+}
